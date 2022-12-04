de14bfa89da4ea038092b134b57b50e1a8ae3895
davidka_neZedster <kdavid161262@yandex.ru>
2022-12-04 17:06:19 +0300

    Portfolio page component, some changes

--- src/components/PortfolioItem/PortfolioItem.jsx
+++ src/components/PortfolioItem/PortfolioItem.jsx
@@ -1,14 +1,27 @@
 import styles from './PortfolioItem.module.css'
 import cn from 'classnames'
 import Image from 'next/image'
+import ArrowDown from '../../shared/assets/icons/common/arrow__down.svg'
 
 export const PortfolioItem = ({ imageUrl, name, subscription, textRight }) => {
   return (
     <div className={styles.work}>
       <div className={styles.work__image}>
-        <Image src={imageUrl} alt='work' />
+        <Image placeholder='blur' priority src={imageUrl} alt='work' />
       </div>
-      <div className={textRight ? cn('textRight', styles.work__title) : cn(styles.work__title)}>{name}</div>
+      <div className={textRight ? cn('textRight', styles.work__title) : cn(styles.work__title)}>
+        {textRight ? (
+          <>
+            <ArrowDown />
+            {name}
+          </>
+        ) : (
+          <>
+            {name}
+            <ArrowDown />
+          </>
+        )}
+      </div>
       <div className={textRight ? cn('textRight', styles.work__subscription) : cn(styles.work__subscription)}>
         {subscription}
       </div>
